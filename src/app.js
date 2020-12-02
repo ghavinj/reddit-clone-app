45f9ab147adb026968c4ba1a5f61fd1f09c4b897
Ghavin <ghavinj@MININT-IP0P19V.crossover.local>
2020-12-02 09:14:23 -0500

    Adds all the necessary code

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
+import React, { useEffect, useState } from "react";
+import Post from "./components/post";
+import Navbar from "./components/navbar";
+import db from "./lib/firebase";
+
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Hook to handle the initial fetching of setPosts
+
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPosts(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    //Hook to handle the real-time updating of posts whenever there is a
+    //change in the datastore (https://firebase.googl.com/docs/firestore/quey-data/listen#view_changes_between_snapshots)
+
+    db.collection("posts")
+    .orderBy("createdAt", "desc")
+    .onSnapshot((querySnapshot) => {
+      const _posts = [];
+
+      querySnapshot.forEach((doc) => {
+          _posts.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+      });
+      setPosts(_posts);
+    });
+  });
+  return (
+    <>
+      <Navbar />
+      <Container maxW="md" centerContent p={8}>
+        <VStack spacing={8} w="100%">
+        {posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
+        </VStack>
+      </Container>
+    </>
+  );
+};
+
+export default App;
